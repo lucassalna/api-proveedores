d4606ba53d6616c85bd2fb22233e06c0bc32d22c
crisgit24 <cristian.camilo.franco@correounivalle.edu.co>
2025-05-18 17:16:42 -0500

    intento de pasar a postgres

--- CasoDeEstudioParalelas/settings.py
+++ CasoDeEstudioParalelas/settings.py
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'CasoDeEstudioParalelas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Nombre de la base de datos
+        'USER': 'postgres',  # Usuario de PostgreSQL
+        'PASSWORD': 'sqlite',
+        'HOST': 'localhost',  # O la dirección de tu servidor PostgreSQL
+        'PORT': '5432',       # Puerto por defecto de PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
